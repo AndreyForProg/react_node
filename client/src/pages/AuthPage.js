9e3a0994fe9c05ad95da51e4585ba947413ab8e4
Andrey Pogorelchenco <andreyforprog@gmail.com>
2020-11-13 04:42:13 +0300

    create base structure app

---
+++ client/src/pages/AuthPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const AuthPage = () => {
+  return (
+    <div className="container">
+      <h1>Auth Page</h1>
+    </div>
+  )
+}
---
+++ client/src/pages/DetailPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const DetailPage = () => {
+  return (
+    <div className="container">
+      <h1>DetailPage Page</h1>
+    </div>
+  )
+}
